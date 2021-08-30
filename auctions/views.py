e5d02f96f96580dd53f93009b99e308a4d043b1b
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-08-29 22:56:15 -0500

    convert Listing to bootstrap card

--- auctions/views.py
+++ auctions/views.py
@@ -93,10 +93,14 @@ def listing(request, album_id):
     album = Album.objects.get(id=album_id)
     genres = album.genres.all()
     watchers = album.watchers.all()
-    watchersIds = map(lambda watcher: watcher.id, watchers)
+    # watchersIds = map(lambda watcher: watcher.id, watchers)
+    watchersIds = []
+    for watcher in watchers:
+        watchersIds.append(watcher.id)
     return render(request, "auctions/listing.html", {
         "album": album,
         "genres": genres,
+        "watchers": watchers,
         "watchersIds": watchersIds,
     })
 
