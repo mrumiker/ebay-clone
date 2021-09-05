26af859a32c65b1cac03ef581322b9271c18b746
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-09-04 22:14:36 -0500

    make Space for descriptions

--- auctions/models.py
+++ auctions/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
 class Genre(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
-    description = models.CharField(max_length=512)
+    description = models.TextField(max_length=1024)
 
     def __str__(self):
         return f"{self.name}"
@@ -22,7 +22,7 @@ class Album(models.Model):
     title = models.CharField(max_length=128)
     artist = models.CharField(max_length=64)
     year = models.CharField(max_length=4)
-    description = models.CharField(max_length=512)
+    description = models.TextField(max_length=1024)
     seller = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="albums_selling")
     image_url = models.URLField(max_length=200, blank=True)
@@ -62,7 +62,7 @@ class Comment(models.Model):
         Album, on_delete=models.CASCADE, related_name="comments")
     commenter = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="comments")
-    text = models.CharField(max_length=1024)
+    text = models.TextField(max_length=1024)
     datetime_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
