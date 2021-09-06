7e4f62099ee64162ebb153270fe706ef98d50d60
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-09-05 23:02:38 -0500

    fix Albums empty list issue

--- auctions/views.py
+++ auctions/views.py
@@ -241,11 +241,14 @@ def delete_from_watchlist(request):
 def albums(request, user_id):
     user = User.objects.get(id=user_id)
     albums_winning = user.winning.all()
-    albums_won = filter(lambda album: album.datetime_closed, albums_winning)
+    albums_won = list(
+        filter(lambda album: album.datetime_closed, albums_winning))
+    selling = user.selling.all()
     return render(request, "auctions/albums.html", {
         "won": albums_won,
-        "selling": user.selling.all(),
+        "selling": selling,
         "featured_user": user,
+        # "selling_length": len(selling),
     })
 
 
